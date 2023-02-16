a0a2954119cc5c788e49e0b415ccf3c855d6db7f
Daniel <mukhametshin.work@gmail.com>
2023-02-17 03:35:13 +0700

    Added port to .env file, changed workflow

--- bot/src/main.ts
+++ bot/src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppLogger } from "./helpers/logger/logger.service";
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true
   });
   app.enableCors()
-  app.useLogger(new AppLogger())
-  await app.listen(3001);
+  const logger = new AppLogger()
+  app.useLogger(logger)
+  const config = app.get<ConfigService>(ConfigService)
+  const port = config.get('PORT')
+  await app.listen(port);
+  logger.log(`Started on :${port}`)
 }
 bootstrap().then();
